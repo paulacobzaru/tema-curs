6b3fdbaae73ff1214ea0f03236609460125a302d
paulacobzaru <paulacobzaru@yahoo.com>
2022-05-08 02:44:26 +0300

    commit2-tema3

--- tema-3/main.js
+++ tema-3/main.js
@@ -1,111 +1,12 @@
-let a = "Jane Smith has $12,450 in her bank account.";
-let b = "$12,450 is a lot of money";
-let c = "Jane Smith plans to buy a Toyota Yaris.";
-let d = "Jane Smith will use her $12,450 to buy a Toyota Yaris.";
-let e = " a Toyota Yaris.";
-let f = b + " for " + e;
+/*Requirements - Tema 3
+1.Tema obigatorie si simpla:
+In proiectul tema-curs, 
+intr-o pagina ce se deschide din meniul Tema 3 
+(vezi tema de la cursul 2), afisati urmatorul text: 
+ATENTIE! Nu se va scrie textul cerut in HTML, 
+vom folosi doar JavaScript (JS)
 
-let theNewPTag1 = document.createElement('p');//<p></p>
-let theText1 = document.createTextNode(a);
-theNewPTag1.appendChild(theText1);//<p>test text</p>
-let element1 = document.getElementsByTagName("body")[0];
-element1.appendChild(theNewPTag1);//<body><p>test text</p></body>
-
-let theNewPTag2 = document.createElement('p');//<p></p>
-let theText2 = document.createTextNode(b + ' .');
-theNewPTag2.appendChild(theText2);//<p>test text</p>
-let element2 = document.getElementsByTagName("body")[0];
-element2.appendChild(theNewPTag2);//<body><p>test text</p></body>
-
-let theNewPTag3 = document.createElement('p');//<p></p>
-let theText3 = document.createTextNode(c);
-theNewPTag3.appendChild(theText3);//<p>test text</p>
-let element3 = document.getElementsByTagName("body")[0];
-element3.appendChild(theNewPTag3);//<body><p>test text</p></body>
-
-let theNewPTag4 = document.createElement('p');//<p></p>
-let theText4 = document.createTextNode(d);
-theNewPTag4.appendChild(theText4);//<p>test text</p>
-let element4 = document.getElementsByTagName("body")[0];
-element4.appendChild(theNewPTag4);//<body><p>test text</p></body>
-
-let theNewPTag5 = document.createElement('p');//<p></p>
-let theText5 = document.createTextNode(b + ' for' + e);
-theNewPTag5.appendChild(theText5);//<p>test text</p>
-let element5 = document.getElementsByTagName("body")[0];
-element5.appendChild(theNewPTag5);//<body><p>test text</p></body>
-
-
-//==================================================
-let theNewSpanTag = document.createElement('span');
-theNewSpanTag.setAttribute('id', 'dots')
-let element6 = document.getElementsByTagName("body")[0];
-element6.appendChild(theNewSpanTag);
-
-let dots = document.getElementById('dots');
-dots.style.visibility = 'hidden';
-let width, savedContent;
-do {
-  savedContent = dots.innerHTML;
-  dots.innerHTML += '=';
-} while(dots.offsetWidth < dots.parentNode.offsetWidth);
-dots.innerHTML = savedContent;
-dots.style.visibility = 'visible';
-
-//==================================================
-
-let theNewPTag6 = document.createElement('p');//<p></p>
-let theText6 = document.getSelection(b);
-theNewPTag6.appendChild(theText6);//<p>test text</p>
-let element7 = document.getElementsByTagName("body")[0];
-element7.appendChild(theNewPTag6);//<body><p>test text</p></body>
-
-let theNewPTag7 = document.createElement('p');//<p></p>
-let theText7 = document.createTextNode(b + ' for ' + e);
-theNewPTag7.appendChild(theText7);//<p>test text</p>
-let element8 = document.getElementsByTagName("body")[0];
-element8.appendChild(theNewPTag7);//<body><p>test text</p></body>
-
-let theNewPTag8 = document.createElement('p');//<p></p>
-let theText8 = document.createTextNode(c);
-theNewPTag8.appendChild(theText8);//<p>test text</p>
-let element9 = document.getElementsByTagName("body")[0];
-element9.appendChild(theNewPTag8);//<body><p>test text</p></body>
-
-let theNewPTag9 = document.createElement('p');//<p></p>
-let theText9 = document.createTextNode(a);
-theNewPTag9.appendChild(theText9);//<p>test text</p>
-let element10 = document.getElementsByTagName("body")[0];
-element10.appendChild(theNewPTag9);//<body><p>test text</p></body>
-
-let theNewPTag10 = document.createElement('p');//<p></p>
-let theText10 = document.createTextNode(d);
-theNewPTag10.appendChild(theText10);//<p>test text</p>
-let element11 = document.getElementsByTagName("body")[0];
-element11.appendChild(theNewPTag10);//<body><p>test text</p></body>
-
-//=================================================
-
-let theNewSpanTag1 = document.createElement('span');
-theNewSpanTag.setAttribute('id', 'dots')
-let element12 = document.getElementsByTagName("body")[0];
-element12.appendChild(theNewSpanTag);
-
-let dots1 = document.getElementById('dots');
-dots1.style.visibility = 'hidden';
-let width1, savedContent1;
-do {
-  savedContent1 = dots1.innerHTML;
-  dots1.innerHTML += '=';
-} while(dots1.offsetWidth < dots1.parentNode.offsetWidth);
-dots1.innerHTML = savedContent1;
-dots1.style.visibility = 'visible';
-
-
-
-
-//Requirements
-/*“Jane Smith has $12,450 in her bank account.” a
+“Jane Smith has $12,450 in her bank account.” a
 “$12,450 is a lot of money” b + “.”
 “Jane Smith plans to buy a Toyota Yaris.” c
 “Jane Smith will use her $12,450 to buy a Toyota Yaris. “ d
@@ -113,8 +14,8 @@ dots1.style.visibility = 'visible';
 
 =============================================
 
-“$12,450 is a lot of money. “ b
-“$12,450 is a lot of money“ b + “ for " + "a Toyota Yaris. “ e 
+“$12,450 is a lot of money“ b + "."
+“$12,450 is a lot of money“ b + “ for " + "a Toyota Yaris. “ e = f
 “Jane Smith plans to buy a Toyota Yaris. “ c
 “Jane Smith has $12,450 in her bank account. “ a
 “Jane Smith will use her $12,450 to buy a Toyota Yaris. “ d
@@ -122,17 +23,115 @@ dots1.style.visibility = 'visible';
 =============================================
 
 “Jane Smith plans to buy“ g +  “a Toyota Yaris. “ e
-“$12,450 is a lot of money“ b + “for” + “a Toyota Yaris.” e
+“$12,450 is a lot of money“ b + “for” + “a Toyota Yaris.” e=f
 “Jane Smith will use her $12,450 to buy a Toyota Yaris.“ d
 “Jane Smith has $12,450 in her bank account.”a
-“$12,450 is a lot of money.” b*/
+“$12,450 is a lot of money.” b
 
+2. Tema obligatorie ce necesita creativitate :)
+Rezolvati exercitiul de mai sus, incercand sa folositi 
+cat mai putin cod JS 
+(masurat in numar de randuri dar si numar de caractere pe rand).
 
+3. Tema optionala ce necesita cautari on-line:
+Cautati online cum se declara si se folosesc functiile in JavaScript. 
+Folositi, apoi, functii pentru scrierea textului de la punctele 1 si 2 ale temei. 
+O functie poate sa fie utilizata pentru a scrie paragrafe intregi. 
+Alte functii pot fi utilizate pentru a scrie cate un rand din paragraf. 
+Parametrii funcției, adica datele pe care functia de primeste, 
+ar putea fi textele comune, puse in variabile.*/
 
-	
-/*Tema obligatorie ce necesita creativitate :)
-Rezolvati exercitiul de mai sus, incercand sa folositi cat mai putin cod JS (masurat in numar de randuri dar si numar de caractere pe rand).
+let a = "Jane Smith has $12,450 in her bank account.";
+let b = "$12,450 is a lot of money";
+let c = "Jane Smith plans to buy a Toyota Yaris.";
+let d = "Jane Smith will use her $12,450 to buy a Toyota Yaris.";
+let e = " a Toyota Yaris.";
+let f = b + " for " + e;
+let g = "="
 
-Tema opțională ce necesita căutări online :)*/
+function createPTag(myText){
+  let theNewPTag = document.createElement('p');//<p></p>
+  let theText = document.createTextNode(myText);//test text
+  document.body.appendChild(theText);//<p>test text</p>
+  let elem = document.getElementsByTagName("body")[0];
+  elem.appendChild(theNewPTag);//<body><p>test text</p></body>
+}
+
+function createBrTag() {
+  //Create an input type dynamically.
+     var br = document.createElement('br');
+     var foo = document.getElementsByTagName("body")[0];
+     foo.appendChild(br);
+ } 
+
+function createSpanTag1(g){
+  let theNewSpan = document.createElement('span');//<span></span>
+  theNewSpan.setAttribute('id','spanId1')
+  let spanContent = document.createTextNode(g);
+  let el = document.getElementsByTagName("body")[0];
+  el.appendChild(theNewSpan);
+  //let mySpanId = document.getElementsById('spanId').style.width = '100%';
+  let equals = document.getElementById('spanId1');
+  //document.getElementsByClass('spanClass');
+  //for(let x = 0; x < equals.length; x++){
+  //  if(equals[x].style.visibility="hidden");
+  //}
+  //document.style.visibility = "hidden";
+  equals.style.visibility = "hidden";
+  let width, savedContent;
+  do{
+    savedContent = equals.textContent;
+    equals.textContent += '=';
+  }
+  while(equals.offsetWidth < equals.parentNode.offsetWidth);
+  equals.textContent = savedContent;
+  equals.style.visibility = 'visible';
+}
+
+function createSpanTag2(g){
+  let theNewSpan = document.createElement('span');//<span></span>
+  theNewSpan.setAttribute('id','spanId2')
+  let spanContent = document.createTextNode(g);
+  let el = document.getElementsByTagName("body")[0];
+  el.appendChild(theNewSpan);
+  let equals = document.getElementById('spanId2');
+  equals.style.visibility = "hidden";
+  let width, savedContent;
+  do{
+    savedContent = equals.textContent;
+    equals.textContent += '=';
+  }
+  while(equals.offsetWidth < equals.parentNode.offsetWidth);
+  equals.textContent = savedContent;
+  equals.style.visibility = 'visible';
+}
+
+let p1 = createPTag(a);
+let p2 = createPTag(b + '.');
+let p3 = createPTag(c);
+let p4 = createPTag(d);
+let p5 = createPTag(f);
+
+let p6 = createBrTag();
+let p7 = createSpanTag1(g);
+let p8 = createBrTag();
+let p9 = createBrTag();
+
+let p10 = createPTag(b + '.');
+let p11 = createPTag(f);
+let p12 = createPTag(c);
+let p13 = createPTag(a);
+let p14 = createPTag(d);
+
+let p15 = createBrTag();
+let p16 = createSpanTag2(g);
+let p17 = createBrTag();
+let p18 = createBrTag();
+
+let p19 = createPTag(c);
+let p20 = createPTag(f);
+let p21 = createPTag(c);
+let p22 = createPTag(d);
+let p23 = createPTag(c);
 
 
