47f0e9825f89b8a80668efd6f49eb139294a96f4
paulacobzaru <paulacobzaru@yahoo.com>
2022-05-08 17:29:23 +0300

    newcommit-tema3

--- tema-3/main.js
+++ tema-3/main.js
@@ -1,21 +1,37 @@
-let a = "Jane Smith has $12,450 in her bank account.";
-let b = "$12,450 is a lot of money.";
-let c = "Jane Smith plans to buy a Toyota Yaris.";
-let d = "Jane Smith will use her $12,450 to buy a Toyota Yaris.";
-let f = "$12,450 is a lot of money for a Toyota Yaris.";
+let name = "Jane Smith";
+let sum = "$12,450";
+let car = "Toyota Yaris";
 let g = "=";
-function createPTag(myText){
+
+let myFunction1 = function nameHasSum(name, sum){
+  return name + " has " + sum + " in her bank account.";
+}
+let myFunction2 = function sumMoney(sum){
+  return sum + " is a lot of money.";
+}
+let myFunction3 = function nameWantsCar(name, car){
+  return name + " plans to buy a " + car + ' .';
+}
+let myFunction4 = function nameUseMoneyCar(name, money, car){
+  return name + " will use her " + sum + " to buy a " + car + '.';
+}
+let myFunction5 = function nameMoneyCar(name, car){
+  return name + " is a lot of money for a " + car + ' .';
+}
+
+let parFunction = function createPTag(myText){
   let theNewPTag = document.createElement('p');//<p></p>
-  let theText = document.createTextNode(myText);//test text
-  document.body.appendChild(theText);//<p>test text</p>
+  theNewPTag.innerHTML = myText;//test text
   let elem = document.getElementsByTagName("body")[0];
   elem.appendChild(theNewPTag);//<body><p>test text</p></body>
 }
+
 function createBrTag() {
-     var br = document.createElement('br');
-     var brPar = document.getElementsByTagName("body")[0];
-     brPar.appendChild(br);
- } 
+  let br = document.createElement('br');
+  let brPar = document.getElementsByTagName("body")[0];
+  brPar.appendChild(br);
+} 
+
 function createSpanTag1(g){
   let theNewSpan = document.createElement('span');//<span></span>
   theNewSpan.setAttribute('id','spanId1')
@@ -33,45 +49,50 @@ function createSpanTag1(g){
   equals.textContent = savedContent;
   equals.style.visibility = 'visible';
 }
-function createSpanTag2(g){
-  let theNewSpan = document.createElement('span');//<span></span>
-  theNewSpan.setAttribute('id','spanId2')
+
+function createSpanTag2(g) {
+  let theNewSpan = document.createElement('span'); //<span></span>
+  theNewSpan.setAttribute('id', 'spanId2');
   let spanContent = document.createTextNode(g);
   let el = document.getElementsByTagName("body")[0];
   el.appendChild(theNewSpan);
   let equals = document.getElementById('spanId2');
   equals.style.visibility = "hidden";
   let width, savedContent;
-  do{
+  do {
     savedContent = equals.textContent;
     equals.textContent += '=';
   }
-  while(equals.offsetWidth < equals.parentNode.offsetWidth);
+  while (equals.offsetWidth < equals.parentNode.offsetWidth);
   equals.textContent = savedContent;
   equals.style.visibility = 'visible';
 }
-createPTag(a);
-createPTag(b);
-createPTag(c);
-createPTag(d);
-createPTag(f);
+
+parFunction(myFunction1('Jane Smith', '$12,450'));
+parFunction(myFunction2('$12,450'));
+parFunction(myFunction3('Jane Smith', 'Toyota Yaris'));
+parFunction(myFunction4('Jane Smith', '$12,450', 'Toyota Yaris'));
+parFunction(myFunction5('$12,450', 'Toyota Yaris'));
+
 createBrTag();
 createSpanTag1(g);
 createBrTag();
 createBrTag();
-createPTag(b);
-createPTag(f);
-createPTag(c);
-createPTag(a);
-createPTag(d);
+
+parFunction(myFunction2('$12,450'));
+parFunction(myFunction5('$12,450', 'Toyota Yaris'));
+parFunction(myFunction3('Jane Smith', 'Toyota Yaris'));
+parFunction(myFunction1('Jane Smith', '$12,450'));
+parFunction(myFunction4('Jane Smith', '$12,450', 'Toyota Yaris'));
+
 createBrTag();
 createSpanTag2(g);
 createBrTag();
 createBrTag();
-createPTag(c);
-createPTag(f);
-createPTag(d);
-createPTag(a);
-createPTag(b);
 
+parFunction(myFunction3('Jane Smith', 'Toyota Yaris'));
+parFunction(myFunction5('$12,450', 'Toyota Yaris'));
+parFunction(myFunction4('Jane Smith', '$12,450', 'Toyota Yaris'));
+parFunction(myFunction1('Jane Smith', '$12,450'));
+parFunction(myFunction2('$12,450'));
 
