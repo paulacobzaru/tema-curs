df75c1875e14abdaf0788a95efa2d7853e30e711
paulacobzaru <paulacobzaru@yahoo.com>
2022-06-21 22:16:34 +0300

    Homework 7 all

--- tema-5/functions.js
+++ tema-5/functions.js
@@ -267,5 +267,29 @@ let resChain = document.createElement('div');
 resChain.innerHTML = chainObject.addNo().substractNo().multiplyNo().devideNo();
 result7.appendChild(resChain);**/
 
-console.log(person.checkIfRomanian('Italian').calculateYourAge('2000'));
+//console.log(person.checkIfRomanian('Italian').calculateYourAge('2000'));
 
+const dog = {
+  is: null,
+  log: () => console.log(this.is),
+  bark() {
+    this.is = "woofing";
+    this.log();
+    return this;
+  },
+  walk() {
+    this.is = "walking";
+    this.log();
+    return this;
+  },
+  eat() {
+    this.is = "eating";
+    this.log();
+    return this;
+  }
+};
+
+dog
+  .bark()
+  .eat()
+  .walk();
