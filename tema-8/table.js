5578fe9a20dd7d87a1ac1eb125920f1f674942c2
paulacobzaru <paulacobzaru@yahoo.com>
2022-06-22 20:18:26 +0300

    Homework 8

--- tema-8/table.js
+++
@@ -1,8 +0,0 @@
-function myCreateFunction() {
-    var table = document.getElementById("myTable");
-    var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    cell1.innerHTML = "NEW CELL1";
-
-  }
